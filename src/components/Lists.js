29aa24be6853d9526b978f8a89085d949c59c1c7
ipperbala6677 <ipperbala6677@gmail.com>
2023-04-07 00:08:07 +0530

    Added Quanitity filed

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
-import { Card, CardBody, CardHeader, Input, Button, Table } from "reactstrap";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Input,
+  Button,
+  Table,
+  Col,
+  CloseButton,
+} from "reactstrap";
 
 const Lists = ({ inventoryData, setInventoryData }) => {
+  const [quantity, setQuantity] = useState("");
   const [inputText, setInputText] = useState("");
   const handleAdd = () => {
     if (!inputText) {
@@ -11,12 +21,13 @@ const Lists = ({ inventoryData, setInventoryData }) => {
     const item = {
       id: Math.floor(Math.random() * 1000),
       name: inputText,
-      quantity: 0,
+      quantity: quantity,
     };
     const tempInventoryData = [...inventoryData];
     tempInventoryData.push(item);
     setInventoryData(tempInventoryData);
     setInputText("");
+    setQuantity("");
   };
 
   const handleDelete = (id) => {
@@ -33,39 +44,68 @@ const Lists = ({ inventoryData, setInventoryData }) => {
       <div className="d-flex justify-content-center">
         <Card className="w-50">
           <CardBody>
-            <div style={{ textAlign: "left", marginBottom: "8px" }}>
-              {" "}
-              Item Name *
+            <div style={{ display: "flex" }}>
+              <span style={{ textAlign: "left", marginRight: "37%" }}>
+                {" "}
+                <h3 style={{ fontSize: "25px" }}>Item Name *</h3>
+              </span>
+              <span style={{ textAlign: "right", marginLeft: "33%" }}>
+                {" "}
+                <h3 style={{ fontSize: "25px" }}>Quantity *</h3>
+              </span>
             </div>
+
             <div style={{ display: "flex" }}>
               <Input
+                style={{ marginRight: "2%", backgroundColor: "#f2f2f2" }}
                 onChange={(e) => setInputText(e.target.value)}
                 value={inputText}
               />
+              <Input
+                style={{ width: "100px", backgroundColor: "#f2f2f2" }}
+                onChange={(e) => setQuantity(e.target.value)}
+                value={quantity}
+              ></Input>
               <Button color="primary" onClick={handleAdd}>
                 Add
               </Button>
             </div>
-            <div style={{ textAlign: "left", marginTop: "8px" }}>
+
+            <div
+              style={{
+                textAlign: "left",
+                marginTop: "8px",
+                marginBottom: "8px",
+              }}
+            >
               To get started, add 1 or more items
             </div>
-            <Table striped className="">
-              <thead>
-                <tr>
-                  <th>Inventory List</th>
-                  <th></th>
-                </tr>
-              </thead>
+
+            <h3
+              style={{
+                textAlign: "left",
+                fontSize: "25px",
+                padding: "10px",
+                backgroundColor: "#f2f2f2",
+                width: "100%",
+                marginTop: "15px",
+              }}
+            >
+              Inventory List
+            </h3>
+            <Table className="">
               <tbody>
                 {inventoryData.map((item) => (
                   <tr>
                     <th scope="row">{item.name}</th>
+                    <td>Quantity: {item.quantity}</td>
+
                     <td>
                       <div
                         style={{ cursor: "pointer", color: "#d73838" }}
                         onClick={() => handleDelete(item.id)}
                       >
-                        X
+                        <b>X</b>
                       </div>
                     </td>
                   </tr>
@@ -73,7 +113,11 @@ const Lists = ({ inventoryData, setInventoryData }) => {
               </tbody>
             </Table>
             <div style={{ display: "flex", justifyContent: "end" }}>
-              <Button outline onClick={handleClearAll}>
+              <Button
+                style={{ paddingInline: "5% 5%" }}
+                outline
+                onClick={handleClearAll}
+              >
                 Clear All
               </Button>
             </div>
